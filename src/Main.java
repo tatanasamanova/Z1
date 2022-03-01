f21e8b131483dfb9ef8c0bc72a57390aa9a5717e
tatana <tatanasamanova38@gmail.com>
2022-03-01 20:08:05 +0300

    TEXT

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main
+
+{
+
+    public static void main(String[] args) {
+
+        String text="Уже тепло, но в то же время холодно.";
+
+        int i=text.indexOf(".");
+
+        System.out.println("Всего символов:"+i);
+
+    }
+
+}
